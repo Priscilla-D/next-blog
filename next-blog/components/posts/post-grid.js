4db986754d8939d5b3b2d7993b6c49b71bbf97ff
Priscilla-placeshaker <92723280+Priscilla-placeshaker@users.noreply.github.com>
2021-11-26 15:56:23 +0100

    section 11-183

--- next-blog/components/posts/post-grid.js
+++ next-blog/components/posts/post-grid.js
@@ -1,11 +1,15 @@
-import PostItem from'./post-item';
-import classes from './posts-grid.module.css';
+import PostItem from "./post-item";
+import classes from "./posts-grid.module.css";
 
 function PostsGrid(props) {
-    const { posts } = props;
-return <ul className={classes.grid}>
-    {posts.map(post => <PostItem />)}
-</ul>
+  const { posts } = props;
+  return (
+    <ul className={classes.grid}>
+      {posts.map((post) => (
+        <PostItem key={post.slug} post={post}/>
+      ))}
+    </ul>
+  );
 }
 
 export default PostsGrid;
--- next-blog/components/posts/post-item.js
+++ next-blog/components/posts/post-item.js
@@ -1,20 +1,27 @@
 import Link from "next/link";
-import Image from 'next/image';
+import Image from "next/image";
 import classes from "./post-item.module.css";
 
 function PostItem(props) {
-    const{ title, image, excerpt, date, slug }= props.post;
-     const formattedDate = new Date(date).toLocaleDateString('en-US', {
-         day: 'numeric',
-         month: 'long',
-         year: 'numeric'
-     });
+  const { title, image, excerpt, date, slug } = props.post;
+  const formattedDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  const imagePath = `/images/posts/${slug}/${image}`;
   return (
     <li className={classes.post}>
-      <Link>
+      <Link href={`/posts/${slug}`}>
         <a>
           <div className={classes.image}>
-            <Image src={`/images/posts/${slug}/${image}`} alt={title} width={300} height={200} />
+            <Image
+              src={imagePath}
+              alt={title}
+              width={300}
+              height={200}
+              layout="responsive"
+            />
           </div>
           <div className={classes.content}>
             <h3>{title}</h3>
